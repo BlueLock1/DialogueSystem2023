6a0d8ba9065c120c8ce408090514f1cc690451aa
deathstrider941 <BRUCE.JIMENEZPABLO@GMAIL.COM>
2023-02-18 14:24:42 -0800

    class 1

---
+++ DialogueSystemClass/Assets/Scripts/DialogieDatabase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue System/Dialogue Data")]
+public class DialogieDatabase : ScriptableObject
+{
+    public DialogueLineData[] data;
+}
